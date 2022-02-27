678d9db167ecb1697efeedb8d6bdfc0207c5ea02
Zach Butler <kj4ezj@pm.me>
2022-02-27 14:16:22 -0500

    Create function for git pull and to update submodules

--- git.sh
+++ git.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 alias apply='git cherry-pick'
+
+function pull()
+{
+    git pull origin "$(git branch --show-current)"
+    git submodule update --init --recursive
+}
+
 alias status='git status'
